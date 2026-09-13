2d8555d7fe55a51f652b007700f48b5acffeac46
adityaraturi25 <aditya.raturi25@gmail.com>
2026-09-13 13:02:15 +1000

    Add Koin dependency injection dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+    implementation(platform(libs.koin.bom))
+    implementation(libs.koin.android)
 }
